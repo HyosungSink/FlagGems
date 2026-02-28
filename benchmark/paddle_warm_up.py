503dc9baa6aac74accb8f8bef89c976d6cd08a53
fxyfxy777 <fxyfxy777@163.com>
2026-02-28 15:13:39 +0800

    add warm

---
+++ benchmark/paddle_warm_up.py
@@ -0,0 +1,70 @@
+
+
+_occupy_process = None
+_stop_event = None
+_started = False
+
+
+def occupy_gpu(gpu_id, size, ready_event, stop_event):
+    import paddle
+    import time
+
+    paddle.device.set_device(f"gpu:{gpu_id}")
+    stream = paddle.device.Stream(priority=2)
+
+    x = paddle.ones([size, size])
+
+    with paddle.device.stream_guard(stream):
+        x = x * 2 - 1
+
+    paddle.device.synchronize()
+    ready_event.set()
+
+    while not stop_event.is_set():
+        with paddle.device.stream_guard(stream):
+            x = x * 2 - 1
+            # time.sleep(0.001)
+
+
+def warmup_paddle():
+    import multiprocessing as mp
+    import paddle
+    import os
+    import atexit
+    import time
+    global _occupy_process, _stop_event, _started
+
+    # ✅ 如果已经启动过，就直接返回
+    if _started and _occupy_process is not None and _occupy_process.is_alive():
+        print("GPU already warmed up")
+        return
+
+    mp.set_start_method("spawn", force=True)
+
+    gpu_id = paddle.cuda.current_device()
+    size = 1
+
+    ready_event = mp.Event()
+    _stop_event = mp.Event()
+
+    _occupy_process = mp.Process(
+        target=occupy_gpu,
+        args=(gpu_id, size, ready_event, _stop_event),
+    )
+    _occupy_process.start()
+
+    ready_event.wait()
+    _started = True
+
+    print("GPU warm up success")
+
+    # 自动清理（只注册一次）
+    def cleanup():
+        global _occupy_process, _stop_event, _started
+        if _occupy_process is not None and _occupy_process.is_alive():
+            print("exit!")
+            _stop_event.set()
+            _occupy_process.join()
+        _started = False
+
+    atexit.register(cleanup)
