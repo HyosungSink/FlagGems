4e7b7f8d5729dc754c95fce6186e5e8f2656587c
ldwang <ftgreat@163.com>
2026-04-01 11:09:09 +0800

    [KernelGen] Add binary scalar pointwise benchmarks and Ops (#2158)
    * [KernelGen] Add binary scalar pointwise benchmarks
    
    * [KernelGen] Add add on Ascend
    
    * [KernelGen] Add index_put on Ascend
    
    * [KernelGen] Add lt on Ascend
    
    * [KernelGen] Add ne on Ascend
    
    * [KernelGen] Add topk on Ascend
    
    * [KernelGen] Fix index_put coreDim overflow on Ascend
    
    * [KernelGen] update as review suggestions.
    
    Signed-off-by: ftgreat <ldwang@baai.ac.cn>
    
    ---------
    
    Signed-off-by: ftgreat <ldwang@baai.ac.cn>
    Signed-off-by: ldwang <ftgreat@163.com>

--- src/flag_gems/runtime/backend/_ascend/ops/__init__.py
+++ src/flag_gems/runtime/backend/_ascend/ops/__init__.py
@@ -1,3 +1,4 @@
+from .add import add, add_
 from .addmm import addmm
 from .all import all, all_dim, all_dims
 from .amax import amax
@@ -26,10 +27,12 @@ from .groupnorm import group_norm, group_norm_backward
 from .hstack import hstack
 from .index import index
 from .index_add import index_add
+from .index_put import index_put, index_put_
 from .index_select import index_select
 from .isin import isin
 from .linspace import linspace
 from .log_softmax import log_softmax, log_softmax_backward
+from .lt import lt, lt_scalar
 from .masked_fill import masked_fill, masked_fill_
 from .masked_select import masked_select
 from .max import max, max_dim
@@ -37,6 +40,7 @@ from .mean import mean, mean_dim
 from .min import min, min_dim
 from .mm import mm
 from .multinomial import multinomial
+from .ne import ne, ne_scalar
 from .nonzero import nonzero
 from .ones import ones
 from .ones_like import ones_like
@@ -58,6 +62,7 @@ from .slice_scatter import slice_scatter
 from .softmax import softmax, softmax_backward
 from .sort import sort
 from .stack import stack
+from .topk import topk
 from .threshold import threshold, threshold_backward
 from .triu import triu
 from .unique import _unique2
@@ -70,6 +75,8 @@ from .zeros import zeros
 from .zeros_like import zeros_like
 
 __all__ = [
+    "add",
+    "add_",
     "addmm",
     "all",
     "all_dim",
@@ -151,9 +158,16 @@ __all__ = [
     "any_dim",
     "angle",
     "multinomial",
+    "lt",
+    "lt_scalar",
+    "ne",
+    "ne_scalar",
     "nonzero",
     "index_add",
+    "index_put",
+    "index_put_",
     "_unique2",
+    "topk",
     "upsample_nearest2d",
     "randperm",
     "scatter_add_",
