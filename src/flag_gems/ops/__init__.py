899e92af6c3ccd5d6c4d4799b29850138689b945
finneyyan <yanfengnian123@gmail.com>
2026-04-09 06:38:44 +0000

    Add op: new_full

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -190,6 +190,7 @@ from flag_gems.ops.mv import mv
 from flag_gems.ops.nan_to_num import nan_to_num
 from flag_gems.ops.ne import ne, ne_scalar
 from flag_gems.ops.neg import neg, neg_
+from flag_gems.ops.new_full import new_full
 from flag_gems.ops.nll_loss_nd import nll_loss_nd_backward, nll_loss_nd_forward
 from flag_gems.ops.nllloss import (
     nll_loss2d_backward,
@@ -546,6 +547,7 @@ __all__ = [
     "multinomial",
     "mv",
     "nan_to_num",
+    "new_full",
     "ne",
     "ne_scalar",
     "neg",
